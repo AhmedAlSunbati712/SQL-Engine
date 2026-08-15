2a3025a66d93869f5a275e4d5426d04befc9fe72
AhmedAlSunbati712 <ahmed.m.al.sunbati.27@dartmouth.edu>
2026-08-14 22:02:38 -0700

    feat: Lock Manager interface

---
+++ include/LockManager/LockManager.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <cstddef>
+#include <array>
+#include <LockShard.h>
+#include <cstdint>
+#include <Transaction.h>
+#include <Key.h>
+static constexpr std::size_t SHARD_COUNT = 64;
+enum class LockManagerStatus : std::uint8_t {
+    Success = 0,
+    Busy
+};
+
+class LockManager {
+    public:
+        explicit LockManager() = default;
+        ~LockManager() = default;
+
+        LockManager(const LockManager&) = delete;
+        LockManager& operator=(const LockManager&) = delete;
+
+        LockManager(LockManager&&) = delete;
+        LockManager& operator=(LockManager&&) = delete;
+
+        LockManagerStatus lock_shared(TransactionId txn_id, Key& key);
+        LockManagerStatus lock_exclusive(TransactionId txn_id, Key& key);
+
+        LockManagerStatus unlock_shared(TransactionId txn_id, Key& key);
+        LockManagerStatus unlock_exclusive(TransactionId txn_id, Key& key);
+
+    private:
+        std::array<LockShard, SHARD_COUNT> shards;
+};
